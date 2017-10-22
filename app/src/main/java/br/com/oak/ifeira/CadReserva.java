2941da91ecbe437248cd03564b1d47f1e9ef9197
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2017-10-22 20:41:08 -0200

    Criação das activities

---
+++ app/src/main/java/br/com/oak/ifeira/CadReserva.java
@@ -0,0 +1,13 @@
+package br.com.oak.ifeira;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadReserva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_reserva);
+    }
+}
