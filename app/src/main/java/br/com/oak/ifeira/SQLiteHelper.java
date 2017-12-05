5f15e32f38e1f5323d385f88672bc19904c0b47c
FrancescoBeghelli <francesco_jf@hotmail.com>
2017-12-05 15:33:24 -0200

    Contract e SQLiteHelper

--- app/src/main/java/br/com/oak/ifeira/SQLiteHelper.java
+++ app/src/main/java/br/com/oak/ifeira/SQLiteHelper.java
@@ -2,14 +2,17 @@ package br.com.oak.ifeira;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by Guilherme on 03/12/2017.
  */
 
-public class SQLiteHelper {
+public class SQLiteHelper extends SQLiteOpenHelper{
 
     private Context ctx;
+    private static final int DATABASE_VERSION = 1;
+
     public SQLiteHelper(Context ctx)
     {
         this.ctx = ctx;
@@ -18,5 +21,20 @@ public class SQLiteHelper {
     public SQLiteDatabase getDb()
     {
         return SQLiteDatabase.openOrCreateDatabase(ctx.getDatabasePath("db_ifeira"),null);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        db.execSQL(iFeiraContract.Participante.SQL_CREATE_PARTICIPANTE);
+        db.execSQL(iFeiraContract.Livro.SQL_CREATE_LIVRO);
+        db.execSQL(iFeiraContract.Reserva.SQL_CREATE_RESERVA);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        db.execSQL(iFeiraContract.Participante.SQL_DROP_PARTICIPANTE);
+        db.execSQL(iFeiraContract.Livro.SQL_DROP_LIVRO);
+        db.execSQL(iFeiraContract.Reserva.SQL_DROP_RESERVA);
+        onCreate(db);
     }
 }
